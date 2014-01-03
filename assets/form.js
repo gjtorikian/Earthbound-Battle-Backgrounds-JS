2dc169ad7f9f60db463017620cce3b4600206fdc
Garen Torikian <gjtorikian@gmail.com>
2014-01-03 14:16:54 -0800

    Fix iOS detection typos

--- assets/form.js
+++ assets/form.js
@@ -6,7 +6,7 @@ var suggestedLayers = {
 var layer1, layer2, suggested, aspectRatio, frameskip, read_bgs_dat;
 
 document.addEventListener('DOMContentLoaded', function() {
-  if (true || navigator.userAgent.match('CriOS')) {
+  if ( /(iPad|iPhone|iPod)/g.test(navigator.userAgent) && !/CriOS/.test(navigator.userAgent)) {
     document.getElementById("ios-message").className = 
       document.getElementById("ios-message").className.replace( /hidden/ , '' );
   }
