cb4ffb2fc6a5d6e34f8538dc6a5bfa1a9fb66c9f
Garen J. Torikian <gjtorikian@users.noreply.github.com>
2017-07-27 18:28:31 -0500

    Use truncated file for quicker load

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import ROM from "./rom/rom";
-import data from "../data/backgrounds.dat";
+import data from "../data/truncated_backgrounds.dat";
 export Engine from "./engine";
 export BackgroundLayer from "./rom/background_layer";
 const backgroundData = new Uint8Array(Array.from(data).map(x => x.charCodeAt(0)));
