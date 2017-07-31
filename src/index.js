c2e3c2b17b2e418f85d033a95c8260ee1c462332
Garen J. Torikian <gjtorikian@users.noreply.github.com>
2017-07-31 14:38:23 -0700

    Boot properly

--- src/index.js
+++ src/index.js
@@ -1,6 +1,21 @@
 import ROM from "./rom/rom";
 import data from "../data/truncated_backgrounds.dat";
-export Engine from "./engine";
-export BackgroundLayer from "./rom/background_layer";
+import Engine from "./engine";
+import BackgroundLayer from "./rom/background_layer";
 const backgroundData = new Uint8Array(Array.from(data).map(x => x.charCodeAt(0)));
 new ROM(backgroundData);
+
+// document.addEventListener("DOMContentLoaded", function(event) {
+/* Create two layers */
+
+document.BackgroundLayer = BackgroundLayer;
+
+const layer1 = new document.BackgroundLayer(270);
+const layer2 = new document.BackgroundLayer(269);
+/* Create animation engine  */
+const engine = new Engine([layer1, layer2], {
+	canvas: document.querySelector("canvas")
+});
+document.engine = engine;
+document.engine.animate();
+// });
