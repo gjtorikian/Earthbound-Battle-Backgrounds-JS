8f5de320e7472c88da381d0da3570a867ecac544
Garen J. Torikian <gjtorikian@users.noreply.github.com>
2018-08-11 09:35:52 -0400

    Express upgrade

--- server.js
+++ server.js
@@ -1,20 +1,13 @@
 let express = require('express')
 let app = express()
+let path = require('path')
 const PORT = 8888
 
-app.get('/', function (req, res) {
-  res.redirect('index.html')
-})
+app.use('/assets', express.static(path.join(__dirname, '/assets')))
+app.use('/dist', express.static(path.join(__dirname, '/dist')))
 
-app.configure(function () {
-  app.use(express.methodOverride())
-  app.use(express.bodyParser())
-  app.use(express.static(__dirname))
-  app.use(express.errorHandler({
-    dumpExceptions: true,
-    showStack: true
-  }))
-  app.use(app.router)
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'index.html'))
 })
 
 app.listen(PORT)
