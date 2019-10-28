c7d4811e6031c2621089d73e823a2a2eb47d7314
Garen J. Torikian <gjtorikian@users.noreply.github.com>
2019-10-28 21:30:57 +0000

    Add endless random

--- assets/utils.js
+++ assets/utils.js
@@ -1,12 +1,11 @@
 // give me some JSON based on the "?" params
 function getJsonFromUrl() {
   var query = location.search.substr(1);
-  if (query === "")
-    return "";
+  if (query === "") return "";
 
   var data = query.split("&");
   var result = {};
-  for (var i=0; i<data.length; i++) {
+  for (var i = 0; i < data.length; i++) {
     var item = data[i].split("=");
     result[item[0]] = item[1];
   }
@@ -21,7 +20,7 @@ function setUrlFromString(value) {
   }
 
   var data = value.split("=");
-  var result = []
+  var result = [];
   currentUrlJson[data[0]] = data[1];
 
   for (var key in currentUrlJson) {
@@ -37,22 +36,20 @@ function parseLayerParam(number, options) {
   var defaultLayer = options.firstLayer ? 270 : 269;
   var canvas = document.querySelector("canvas");
   var num = Number(number);
-  if (isNaN(num))
-    num = defaultLayer;
-  else if (num < 0 || num > 326)
-    num = defaultLayer;
+  if (isNaN(num)) num = defaultLayer;
+  else if (num < 0 || num > 326) num = defaultLayer;
 
-  options.firstLayer ? (canvas.dataset.layerOne = num) : (canvas.dataset.layerTwo = num);
+  options.firstLayer
+    ? (canvas.dataset.layerOne = num)
+    : (canvas.dataset.layerTwo = num);
   return num;
 }
 
 function parseFrameskipParam(number) {
   var canvas = document.querySelector("canvas");
   var num = Number(number);
-  if (isNaN(num))
-    return num = 1;
-  else if (num < 1 || num > 10)
-    return num = 1;
+  if (isNaN(num)) return (num = 1);
+  else if (num < 1 || num > 10) return (num = 1);
 
   canvas.dataset.frameskip = num;
   return num;
@@ -61,10 +58,8 @@ function parseFrameskipParam(number) {
 function parseAspectRatioParam(number) {
   var canvas = document.querySelector("canvas");
   var num = Number(number);
-  if (isNaN(num))
-    return num = 0;
-  else if (num != 0 && num != 16 && num != 48 && num != 64)
-    return num = 0;
+  if (isNaN(num)) return (num = 0);
+  else if (num != 0 && num != 16 && num != 48 && num != 64) return (num = 0);
 
   canvas.dataset.aspectRatio = num;
   return num;
--- src/index.js
+++ src/index.js
@@ -2,33 +2,34 @@ import Rom from "./rom/rom";
 import data from "../data/truncated_backgrounds.dat";
 import Engine from "./engine";
 import BackgroundLayer from "./rom/background_layer";
-const backgroundData = new Uint8Array(Array.from(data).map(x => x.charCodeAt(0)));
+const backgroundData = new Uint8Array(
+  Array.from(data).map(x => x.charCodeAt(0))
+);
 
-export const ROM = new Rom(backgroundData)
+export const ROM = new Rom(backgroundData);
 
-var setupEngine = exports.setupEngine = function setupEngine() {
-  let params = getJsonFromUrl()
-  let loader = null
+var setupEngine = (exports.setupEngine = function setupEngine() {
+  let params = getJsonFromUrl();
+  let loader = null;
 
-  let layer1Val = parseLayerParam(params.layer1, { firstLayer: true })
-  let layer2Val = parseLayerParam(params.layer2, { firstLayer: false })
-  let frameskip = parseFrameskipParam(params.frameskip)
-  let aspectRatio = parseAspectRatioParam(params.aspectRatio)
-  parseFullscreen(params.fullscreen)
-
+  let layer1Val = parseLayerParam(params.layer1, { firstLayer: true });
+  let layer2Val = parseLayerParam(params.layer2, { firstLayer: false });
+  let frameskip = parseFrameskipParam(params.frameskip);
+  let aspectRatio = parseAspectRatioParam(params.aspectRatio);
+  parseFullscreen(params.fullscreen);
   let debug = params.debug === "true";
 
-  let fps = 30
-  let alpha = parseFloat(0.5)
+  let fps = 30;
+  let alpha = parseFloat(0.5);
 
   if (layer2Val === 0) {
-    alpha = parseFloat(1.0)
+    alpha = parseFloat(1.0);
   }
 
   // Create two layers
-  document.BackgroundLayer = BackgroundLayer
-  const layer1 = new document.BackgroundLayer(layer1Val, ROM)
-  const layer2 = new document.BackgroundLayer(layer2Val, ROM)
+  document.BackgroundLayer = BackgroundLayer;
+  const layer1 = new document.BackgroundLayer(layer1Val, ROM);
+  const layer2 = new document.BackgroundLayer(layer2Val, ROM);
 
   // Create animation engine
   const engine = new Engine([layer1, layer2], {
@@ -36,11 +37,11 @@ var setupEngine = exports.setupEngine = function setupEngine() {
     aspectRatio: aspectRatio,
     frameSkip: frameskip,
     alpha: [alpha, alpha],
-    canvas: document.querySelector('canvas')
-  })
+    canvas: document.querySelector("canvas")
+  });
 
-  document.engine = engine
-  document.engine.animate(debug)
-}
+  document.engine = engine;
+  document.engine.animate(debug);
+});
 
 setupEngine();
