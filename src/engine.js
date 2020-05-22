292edca4d080de8432415d25e0d6a441e095dc06
Will Marsman <william.marsman@mdacorporation.com>
2020-05-22 17:16:59 -0300

    gh-pages: WIP: Earthbound battle background recorder class.

--- src/engine.js
+++ src/engine.js
@@ -1,6 +1,72 @@
 let frameID = -1
 export const SNES_WIDTH = 256
 export const SNES_HEIGHT = 224
+
+class Recorder {
+  constructor() {
+    this.setToStart = false;
+    this.recording = false;
+    this.done = false;
+    this.chunks = [];
+    this.rec = {};
+    this.ele = {};
+  }
+
+  record(ele) {
+    if (this.recording === false && this.setToStart === false) {
+      console.log("Recording beginning on next path");
+      this.setToStart = true;
+      this.recording = false;
+      this.done = false;
+      this.ele = ele;
+    } else {
+      console.log("Recording in progress, not resetting");
+    }
+  }
+
+  isReadyToStart() {
+    return this.setToStart && !this.recording;
+  }
+
+  setAsRecordingBegan(canvas) {
+    this.setToStart = false;
+    this.recording = true;
+    this.chunks = []; // here we will store our recorded media chunks (Blobs)
+    const stream = canvas.captureStream(); // grab our canvas MediaStream
+    this.rec = new MediaRecorder(stream); // init the recorder
+
+    // every time the recorder has new data, we will store it in our array
+    this.rec.ondataavailable = e => chunks.push(e.data);
+    // only when the recorder stops, we construct a complete Blob from all the chunks
+    this.rec.onstop = e => exportVid(new Blob(chunks, {type: 'video/webm'}));
+    this.rec.start();
+
+  }
+
+  exportVid(blob) {
+    const vid = document.createElement('video');
+    vid.src = URL.createObjectURL(blob);
+    vid.controls = true;
+    this.ele.appendChild(vid);
+    const a = document.createElement('a');
+    a.download = 'earthbound-battle-background.webm';
+    a.href = vid.src;
+    a.textContent = 'download the earthbound battle background.';
+    this.ele.appendChild(a);
+  }
+
+  isReadyToEnd() {
+    return this.recording && !this.done;
+  }
+
+  setAsRecordingDone() {
+    this.recording = false;
+    this.done = true;
+    this.rec.stop();
+  }
+
+}
+
 export default class Engine {
   constructor (layers = [], opts) {
     this.layers = layers
@@ -10,6 +76,7 @@ export default class Engine {
     this.alpha = opts.alpha
     this.canvas = opts.canvas
     this.tick = 0
+    this.recorder = new Recorder()
   }
   animate (debug) {
     let then = Date.now()
@@ -45,6 +112,11 @@ export default class Engine {
         this.tick += this.frameSkip
         if (this.tick > this.fps * 2) {
           this.tick = 0;
+          if (this.recorder.isReadyToStart()) {
+            this.recorder.setAsRecordingBegan(canvas);
+          } else if (this.recorder.isReadyToEnd()) {
+            this.recorder.setAsRecordingDone();
+          }
         }
         image.data.set(bitmap)
         context.putImageData(image, 0, 0)
@@ -57,8 +129,6 @@ export default class Engine {
   }
 
   save(htmlElementToAttachTo) {
-    const beginSave=true;
-    const notDoneSave=true;
-    console.log('beginsave and not done save true');
+    this.recorder.record(htmlElementToAttachTo);
   }
 }
